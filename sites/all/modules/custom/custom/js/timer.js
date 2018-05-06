358eba9428b3602183f36812f73eb8557343fbdb
Senya <suharchuk.dima@gmail.com>
2018-05-06 13:24:14 +0300

    Added custom module with main functionality

---
+++ sites/all/modules/custom/custom/js/timer.js
@@ -0,0 +1,31 @@
+(function ($) {
+    Drupal.behaviors.timer = {
+        attach: function (context) {
+            const timer = $('.active-timer', context).text().split(':');
+            let min = Number.parseInt(timer[0]);
+            let sec = Number.parseInt(timer[1]);
+            if (min) {
+                const interval = setInterval(function () {
+                    sec--;
+
+                    if (sec < 0) {
+                        sec = 59;
+                        min--;
+
+                    }
+
+                    $('.active-timer', context).text(numberToTimeFormat(min) + ':' + numberToTimeFormat(sec));
+
+                    if (min <= 0 && sec <= 0) {
+                        clearInterval(interval);
+                        window.location.reload()
+                    }
+                }, 1000);
+            }
+        }
+    };
+
+    function numberToTimeFormat(n) {
+        return n < 10 ? '0' + n : n;
+    }
+})(jQuery);
