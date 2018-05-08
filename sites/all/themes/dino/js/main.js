2931e799e14f35fa796c4a2fdc5777149bd47719
Senya <suharchuk.dima@gmail.com>
2018-05-08 13:38:11 +0300

    Finished work with food crafting. Added fieldsets for food items of different level. Small updates for animations

--- sites/all/themes/dino/js/main.js
+++ sites/all/themes/dino/js/main.js
@@ -5,8 +5,21 @@
             $('.cook-wrapper input').attr('disabled', true);
 
             $('.user-food-block input[type=checkbox]', context).on('click', function () {
+                var target_fieldset = $(this).closest('fieldset');
                 $(this).parent().find('img').toggleClass('checked');
                 var checkedCount = $('input:checkbox:checked').length;
+
+                // Prevent choose items from other range.
+                if (checkedCount > 0) {
+                    $(target_fieldset).siblings().find('input:checkbox').attr('disabled', true);
+                    $(target_fieldset).siblings().find('img').addClass('muted');
+                }
+                else {
+                    $(target_fieldset).siblings().find('input:checkbox').attr('disabled', false);
+                    $(target_fieldset).siblings().find('img').removeClass('muted');
+                }
+
+                // Show button if chosen 3 items.
                 if (checkedCount === 3) {
                     $('.cook-wrapper').addClass('active');
                     $('.cook-wrapper input').attr('disabled', false);
@@ -16,5 +29,5 @@
                 }
             });
         }
-    }
+    };
 })(jQuery);
