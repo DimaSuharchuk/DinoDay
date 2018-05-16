770fdc123fb13614cb8f6c5809fdaadf0be42aa7
Senya <suharchuk.dima@gmail.com>
2018-05-16 22:51:11 +0300

    Stylized undefined dinosaurs

--- sites/all/themes/dino/templates/dinosaur-undefined-info.tpl.php
+++ sites/all/themes/dino/templates/dinosaur-undefined-info.tpl.php
@@ -1,10 +1,9 @@
 <div id="dinosaur-info">
     <div class="dinosaur-picture-wrapper columns medium-6">
-        <img src="<?php print $picture_path; ?>"
-             class="<?php print $style; ?>"/>
+        <img src="<?php print $picture_path; ?>" class="no-rank"/>
     </div>
     <div class="dinosaur-main-info columns medium-6">
-        <h2 class="dinosaur-name">Неизвесный динозавр</h2>
+        <h2 class="dinosaur-name">Неизвестный динозавр</h2>
         <p>Ранг: <strong><?php print $grade; ?></strong></p>
     </div>
 </div>
