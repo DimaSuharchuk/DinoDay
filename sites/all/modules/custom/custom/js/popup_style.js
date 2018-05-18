3d2a58e9b464552a925d104ff3fb55b37fc564b7
Senya <suharchuk.dima@gmail.com>
2018-05-18 15:46:21 +0300

    Stylized authorization and registration as modal popup

---
+++ sites/all/modules/custom/custom/js/popup_style.js
@@ -0,0 +1,55 @@
+(function ($) {
+    // Initialize nanoScroller.
+    Drupal.behaviors.nanoScrollerInit = {
+        attach: function () {
+            $('.nano').nanoScroller();
+        }
+    };
+
+    Drupal.theme.prototype.customPopupTheme = function () {
+        return '<div id="ctools-modal" class="popups-box dino-popup">' +
+            '     <div class="ctools-modal-content ctools-modal-dino-popup">' +
+            '       <div class="modal-header">' +
+            '         <span class="popups-close">' +
+            '           <a class="close" href="#">' + Drupal.CTools.Modal.currentSettings.closeText + '</a>' +
+            '         </span>' +
+            '         <span id="modal-title" class="modal-title"></span>' +
+            '       </div>' +
+            '       <div class="modal-msg"></div>' +
+            '       <div class="modal-scroll content-wrapper">' +
+            '         <div class="nano">' +
+            '           <div id="modal-content" class="modal-content popup-body nano-content"></div>' +
+            '         </div>' +
+            '       </div>' +
+            '     </div>' +
+            '   </div>';
+    };
+
+    Drupal.behaviors.addModalCloseIcon = {
+        attach: function () {
+            $('#modalBackdrop').html('<i class="material-icons popup-close">close</i>');
+        }
+    };
+
+    Drupal.behaviors.alignPopup = {
+        attach: function () {
+            if ($('#modalContent').length) {
+                setTimeout(function () {
+                    modalContentResize();
+                    $('#modalBackdrop').css({height: '100%', width: '100%'});
+                }, 100);
+                // Prevent ugly page background scroll.
+                $('body .page').css('position', 'fixed');
+            }
+        }
+    };
+
+    Drupal.behaviors.closePopups = {
+        attach: function () {
+            $('#modalBackdrop, .close').bind('click', function () {
+                modalContentClose();
+                $('body .page').css('position', 'relative');
+            });
+        }
+    };
+})(jQuery);
