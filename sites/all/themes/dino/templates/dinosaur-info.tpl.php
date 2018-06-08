dd63549d99d35cd3b1f37dd02a88b11c07abdc52
Senya <suharchuk.dima@gmail.com>
2018-06-08 15:57:45 +0300

    Added translations for dinosaurs and food

--- sites/all/themes/dino/templates/dinosaur-info.tpl.php
+++ sites/all/themes/dino/templates/dinosaur-info.tpl.php
@@ -4,7 +4,7 @@
              class="<?php print $style; ?>"/>
     </div>
     <div class="dinosaur-main-info columns medium-6">
-        <h2 class="dinosaur-name"><?php print $name; ?></h2>
+        <h2 class="dinosaur-name"><?php print t($name); ?></h2>
 
         <div class="dinosaur-bars">
             <div class="dinosaur-life-wrapper">
@@ -17,7 +17,7 @@
 
         <p><?php print t('Rank'); ?>: <strong><?php print $grade; ?></strong></p>
         <p><?php print t('Age'); ?>: <strong><?php print $age; ?></strong></p>
-        <p><?php print t('Food type'); ?>: <strong><?php print $food_type; ?></strong></p>
-        <p><?php print t('Favorite food'); ?>: <strong><?php print $favorite_food; ?></strong></p>
+        <p><?php print t('Food type'); ?>: <strong><?php print t($food_type); ?></strong></p>
+        <p><?php print t('Favorite food'); ?>: <strong><?php print t($favorite_food); ?></strong></p>
     </div>
 </div>
